1428839329cc638857a141f2d1e8e4746f52c921
Sujeet Kumar <sujeetkumaryadav202020@gmail.com>
2022-11-13 00:43:19 +0530

    done

--- src/route/user.model.js
+++ src/route/user.model.js
@@ -1,23 +1,48 @@
 const mongoose =require("mongoose")
+/* 
+const dobSchema=new mongoose.Schema({
+    day:{type:Number},
+    month:{type:Number},
+    year:{type:Number}
+})
 
 const userDetailsSchema=new mongoose.Schema({
-    height:{type:number},
-    weight:{type:number},
-    dob:{type:String}
+    height:{type:Number},
+    weight:{type:Number},
+    birth:dobSchema,
+    gender:{type:String,  enum:["Male","Female"] },
+    units:{type:Boolean},
 })
+
 const reportSchema=new mongoose.Schema({
-    height:{type:number},
-    weight:{type:number},
-    dob:{type:String}
+    exercise_name:{type:String},
+    image:{type:String},
+    muscle_name:{type:String}
 })
+
+ */
 
 const userSchema=new mongoose.Schema({
     email:{type:String,require:true,unique:true},
-    password:{type:String},
-    userDetail:{type:Object},
-    message:{type:Array},
-    photos:{type:Array},
-    
+    password:{type:String,default:Date.now},
+    userDetail:{
+        height:{type:Number,default:59},
+        weight:{type:Number,default:50},
+        birth:{
+            day:{type:Number,default:1},
+            month:{type:Number,default:1},
+            year:{type:Number,default:1989}
+        },
+        gender:{type:String,  enum:["Male","Female"] },
+        units:{type:Boolean},
+    },
+    message:[String],
+    photos:[String],
+    report:[{
+        exercise_name:{type:String,default:"yes"},
+        image:{type:String, default:"yes2"},
+        muscle_name:{type:String, default:"yes3"}
+    }]
 
 })
 
--- src/route/user.route.js
+++ src/route/user.route.js
@@ -3,6 +3,8 @@ const User=require("./user.model")
 
 const app=express.Router()
 
+/* only for Admin panel */
+/* get all users */
 app.get("/",async(req,res)=>{
     try {
         let users= await User.find()
@@ -16,6 +18,58 @@ app.get("/",async(req,res)=>{
         }
     } catch (error) {
         res.send(error.message)
+    }
+})
+
+/* delete a user */
+app.delete("/:id", async (req, res) => {
+    let id = req.params.id
+    try {
+        let existing = await User.findByIdAndDelete(id)
+        if(existing){
+            res.send("User deleted successfully")
+        } else {
+            res.send("user not found")
+        }
+    } catch (e) {
+        res.status(404).send(e.message)
+    }
+   
+})
+
+/* admin panel close */
+
+/* create a user */
+app.post("/", async (req, res) => {
+    const {email,userDetail,message,photos,report,password} = req.body
+    try {
+        let existing = await User.findOne({email})
+        if(existing){
+            res.status(404).send("User Existed")
+        } else {
+            let user = await User.create({
+                email,password,userDetail,message,photos,report
+            })
+            res.send({token: `${user._id}:${email}`})
+        }
+    } catch (e) {
+        res.status(404).send(e.message)
+    }
+   
+})
+
+/* get single user */
+app.get("/:id", async (req, res) => {
+    const id = req.params.id;
+    try {
+        let user = await User.findOne({id});
+        if(user){
+            res.send(user)
+        } else {
+            res.status(404).send(`user ${id} not found`)
+        }
+    } catch (e) {
+        res.status(404).send(e.message)
     }
 })
 
